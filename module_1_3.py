d3a08d784029e1a68c6340dc1f6d6212eada831b
Grigor1911 <grixan.1941@mail.ru>
2024-08-09 00:49:48 +0500

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,12 @@
+name = "Grigory"
+print(name)
+age = 25
+print(age)
+age = age + 1
+print(age)
+is_student = True
+print(is_student)
+print("Name:",name)
+print("Age:",age)
+print("New Age:",age)
+print("Is Student:", is_student)
